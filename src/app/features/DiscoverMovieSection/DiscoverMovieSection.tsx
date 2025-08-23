8cd012a754971ec3ab66796bf1fd656228948ee3
Jordan Ayotte <jordanayotte13@outlook.com>
2025-08-23 19:05:31 -0400

    added card swiping effect with framer motion

--- src/app/features/DiscoverMovieSection/DiscoverMovieSection.tsx
+++ src/app/features/DiscoverMovieSection/DiscoverMovieSection.tsx
@@ -53,6 +53,7 @@ export default function DiscoverMovieSection() {
                 <DiscoverMovieMovieCard
                     key={movieData.id}
                     movieData={movieData}
+                    setMovieData={setMovieData}
                 ></DiscoverMovieMovieCard>
             ));
         }
--- src/app/features/DiscoverMovieSection/components/DiscoverMovieMovieCard.tsx
+++ src/app/features/DiscoverMovieSection/components/DiscoverMovieMovieCard.tsx
@@ -1,23 +1,63 @@
-import { TMDBMovie } from "@/app/types";
+import { TMDBDiscoverResponse, TMDBMovie } from "@/app/types";
 import MovieTitle from "@/components/typography/movieTitle";
+import { motion, useMotionValue, useMotionValueEvent, useTransform } from "framer-motion";
 
 interface DiscoverMovieMovieCardProps {
     movieData: TMDBMovie;
+    setMovieData: React.Dispatch<React.SetStateAction<TMDBDiscoverResponse | null>>;
 }
 
-export default function DiscoverMovieMovieCard({ movieData }: DiscoverMovieMovieCardProps) {
+export default function DiscoverMovieMovieCard({
+    movieData,
+    setMovieData,
+}: DiscoverMovieMovieCardProps) {
+    const x = useMotionValue(0);
+
+    useMotionValueEvent(x, "change", (latest) => console.log(latest));
+
+    const opacity = useTransform(x, [-150, 0, 150], [0, 1, 0]);
+    const rotate = useTransform(x, [-150, 150], [-18, 18]);
+
+    const handleDragEnd = () => {
+        if (x.get() > 50) {
+            setMovieData((data) =>
+                data
+                    ? {
+                          ...data,
+                          results: data.results.filter((movie) => movie.id !== movieData.id),
+                      }
+                    : data
+            );
+        } else if (x.get() < -50) {
+            setMovieData((data) =>
+                data
+                    ? {
+                          ...data,
+                          results: data.results.filter((movie) => movie.id !== movieData.id),
+                      }
+                    : data
+            );
+        }
+    };
+
     return (
-        <div
+        <motion.div
+            drag="x"
+            dragConstraints={{ left: 0, right: 0 }}
+            onDragEnd={handleDragEnd}
             data-testid="movie-bg"
             style={{
                 backgroundImage: `linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)), url(https://image.tmdb.org/t/p/w500${movieData?.poster_path})`,
+                x,
+                opacity,
+                rotate,
             }}
-            className="col-start-1 row-start-1 flex h-[100vh] w-[100vw] rounded-xl bg-cover bg-center md:h-[750px] md:w-[500px]"
+            className="col-start-1 row-start-1 flex h-[100vh] w-[100vw] rounded-xl bg-cover bg-center hover:cursor-grab active:cursor-grabbing md:h-[750px] md:w-[500px]"
         >
             <div className="mt-auto p-4">
                 <MovieTitle text={movieData?.original_title} />
                 <p className="mt-4">{movieData?.overview}</p>
             </div>
-        </div>
+        </motion.div>
     );
 }
