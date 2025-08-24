b1b015da07a766a9bd86489a933814c0e2f33748
Jordan Ayotte <jordanayotte13@outlook.com>
2025-08-24 11:34:17 -0400

    basic auth flow setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 import DiscoverMovieSection from "@/app/features/DiscoverMovieSection/DiscoverMovieSection";
-export default function Home() {
+import { redirect } from "next/navigation";
+import { createClient } from "@/utils/supabase/server";
+
+export default async function Home() {
+    const supabase = await createClient();
+    const { data, error } = await supabase.auth.getUser();
+    if (error || !data?.user) {
+        redirect("/login");
+    }
     return (
         <div className="min-h-screen items-center justify-items-center font-sans">
             <main className="flex flex-col items-start">
