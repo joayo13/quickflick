82609da9e32f4d3ab17589a90bc0d26f8a3ff290
Jordan Ayotte <jordanayotte13@outlook.com>
2025-10-04 17:50:08 -0400

    can now "go back" to previous movie if you accidentally swiped

--- src/features/DiscoverMovieDashboard/components/DiscoverMovieResultList.tsx
+++ src/features/DiscoverMovieDashboard/components/DiscoverMovieResultList.tsx
@@ -2,21 +2,43 @@
 import { useDiscardedMovieStore, useMovieStore, useWatchlistStore } from "@/store/useStore";
 import React, { useRef, useState } from "react";
 import DiscoverMovieResultCard from "./DiscoverMovieResultCard";
-import { CircleXIcon, HeartIcon, ListCheckIcon, XIcon } from "lucide-react";
+import { CircleXIcon, HeartIcon, HistoryIcon, ListCheckIcon, XIcon } from "lucide-react";
 import { addMovieToWatchlist } from "../api/addMovieToWatchlist";
 import { TMDBMovie } from "@/types/types";
 import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
 
 export default function DiscoverMovieResultList() {
-    const { movieData } = useMovieStore();
+    const { movieData, setMovieData } = useMovieStore();
     const { setDiscardedMovieData } = useDiscardedMovieStore();
-    const { watchlistData, setWatchlistData } = useWatchlistStore();
+    const { setWatchlistData } = useWatchlistStore();
     const [exitingMovie, setExitingMovie] = useState<{
         id: number;
         type: "save" | "discard";
     } | null>(null);
     const exitX = useRef(0);
+
+    const [prevMovies, setPrevMovies] = useState<TMDBMovie[]>([]);
+
+    function pushPrevMovieIntoMovieData() {
+        setExitingMovie(null);
+        if (prevMovies.length) {
+            setMovieData((prev) =>
+                prev
+                    ? { ...prev, results: prev.results.concat(prevMovies[prevMovies.length - 1]) }
+                    : prev
+            );
+            setPrevMovies((prev) => prev.slice(0, -1));
+        } else {
+            toast("Can't go back any further.", {
+                icon: <CircleXIcon />,
+            });
+        }
+    }
+
+    function addMovieToPrevMovies(movieData: TMDBMovie) {
+        setPrevMovies((prev) => prev.concat(movieData));
+    }
 
     // select the topmost card data
     const currentMovieData = movieData?.results[movieData.results.length - 1] as TMDBMovie;
@@ -30,6 +52,7 @@ export default function DiscoverMovieResultList() {
             setDiscardedMovieData((prev) =>
                 !prev.includes(movieData.id) ? prev.concat(movieData.id) : prev
             );
+            addMovieToPrevMovies(movieData);
         } else if (x <= -30) {
             // save
             setExitingMovie({ id: movieData.id, type: "save" });
@@ -50,9 +73,7 @@ export default function DiscoverMovieResultList() {
                     );
                 }
                 toast(`${movieData.title} added to watchlist.`, { icon: <ListCheckIcon /> });
-                setTimeout(() => {
-                    console.log(watchlistData);
-                }, 500);
+                addMovieToPrevMovies(movieData);
             } catch (error) {
                 // error feedback
                 if (error instanceof Error) {
@@ -91,6 +112,14 @@ export default function DiscoverMovieResultList() {
                 className="absolute top-1/2 -right-36 hidden h-32 w-32 -translate-y-1/2 rounded-full bg-[var(--border)] lg:flex"
             >
                 <XIcon className="size-16 text-red-500" fill="currentColor" />
+            </Button>
+            <Button
+                aria-label="go back to previous movie"
+                className="absolute top-2 left-2 z-30 h-8 w-8 rounded-full"
+                variant="outline"
+                onClick={pushPrevMovieIntoMovieData}
+            >
+                <HistoryIcon />
             </Button>
         </>
     );
